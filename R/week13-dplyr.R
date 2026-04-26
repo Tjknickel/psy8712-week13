6e0f5be7abf106b166b7cbb25c3d926467953019
Tjknickel <knick071@umn.edu>
2026-04-26 16:07:37 -0500

    Finished Data Import and Cleaning

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -15,6 +15,20 @@ conn <- dbConnect(
   sslmode = "require"
 )
 
+dbListTables(conn)
+employees_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_employees")
+testscores_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_testscores")
+offices_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_offices")
+
+write.csv(employees_tbl, "../data/employees.csv")
+write.csv(testscores_tbl, "../data/testscores.csv")
+write.csv(offices_tbl, "../data/offices.csv")
+
+week13_tbl <- employees_tbl %>%
+  inner_join(testscores_tbl, by = "employee_id") %>%
+  left_join(offices_tbl, by = c("city" = "office"))
+
+write.csv(week13_tbl, "../out/week13.csv")
 
 # Visualization 
 
