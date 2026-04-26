391f8d880a6547fb207424d8c1d5949facaf9991
Tjknickel <knick071@umn.edu>
2026-04-26 15:43:40 -0500

    Finished Part 0

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -7,10 +7,10 @@ library(DBI)
 # Data Import and Cleaning 
 conn <- dbConnect(
   Postgres(),
-  user = "",
-  password = "",
-  dbname = "",
-  host = "",
+  user = Sys.getenv("NEON_USER"),
+  password = Sys.getenv("NEON_PW"),
+  dbname = "neondb",
+  host = "ep-billowing-union-am14lcnh-pooler.c-5.us-east-1.aws.neon.tech",
   port = 5432, 
   sslmode = "require"
 )
