072230252b408c0cf3a28f276be913c6ced0a25e
Tjknickel <knick071@umn.edu>
2026-04-27 08:01:35 -0500

    Finished Project

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -5,6 +5,7 @@ library(RPostgres)
 library(DBI)
 
 # Data Import and Cleaning 
+# This section first connects to the database by using dbConnect along with the server information. The username and password were created in the .Renviron file and are thus not stored here for privacy and security reasons. After connecting, three datasets were imported from the database using dbGetQuery and were also saved as csv files. dbListTables returns the names of the datasets that the user has access to, which was used to import the correct datasets. Finally, week13_tbl was created by joining together two of the datasets by a shared column (employee_id) and then joining by another related column from the third datatset (city/office). This final created dataframe was also then saved as a csv file. 
 conn <- dbConnect(
   Postgres(),
   user = Sys.getenv("NEON_USER"),
@@ -28,26 +29,27 @@ week13_tbl <- employees_tbl %>%
   inner_join(testscores_tbl, by = "employee_id") %>%
   left_join(offices_tbl, by = c("city" = "office"))
 
-write.csv(week13_tbl, "../out/week13.csv")
+write.csv(week13_tbl, "../out/week13.csv", row.names = FALSE)
 
 # Analysis 
+# This section uses tidyr and dplyr functions to analysis the week13_tbl data to complete 5 tasks. The first set of tasks were to display the total number of managers, then the total number of unique managers, then a summary of managers by location. The next set of tasks were to display the mean and standard deviation of years of employment by performance group, and finally to display each managers location classification of their office, ID number, and test score in alphabetical order by location type and descending order of test score. Functions used to accomplish these tasks included filter, summarize, group_by, mutate, select, and arrange. 
 week13_tbl %>% 
   filter(manager_hire == "Y") %>% 
-  summarise(total_managers = n())
+  summarize(total_managers = n())
 
 week13_tbl %>% 
   filter(manager_hire == "Y") %>% 
-  summarise(unique_managers = n_distinct(employee_id))
+  summarize(unique_managers = n_distinct(employee_id))
 
 week13_tbl %>% 
   filter(manager_hire == "Y") %>% 
   group_by(city) %>% 
-  summarise(manager_count = n())
+  summarize(manager_count = n())
 
 week13_tbl %>% 
   mutate(performance_group = factor(performance_group, levels = c("Bottom", "Middle", "Top"))) %>% 
   group_by(performance_group) %>% 
-  summarise(
+  summarize(
     mean_years = mean(yrs_employed, na.rm = TRUE),
     sd_years = sd(yrs_employed, na.rm = TRUE)
   )
--- R/week13-sql.R
+++ R/week13-sql.R
@@ -4,6 +4,7 @@ library(RPostgres)
 library(DBI)
 
 # Data Import and Cleaning 
+# This section first connects to the database by using dbConnect along with the server information. The username and password were created in the .Renviron file and are thus not stored here for privacy and security reasons. After connecting, dbExecute was run in order to create a temporary table from the three datasets that currently existed on the database to be used in the analysis, as this new dataset had to combine aspects of the three datasets to filter for employees that had a test score and also include information on office location. This table inner joined by a shared column from the employees and test score datasets (employee_id) and left joined by a related column (city/offices) from the offices dataset. 
 conn <- dbConnect(
   Postgres(),
   user = Sys.getenv("NEON_USER"),
@@ -28,6 +29,7 @@ dbExecute(conn, "
 ")
 
 # Analysis 
+# This section uses SQL functions to complete 5 tasks using the temporary table (week13_tbl) data. The first set of tasks were to display the total number of managers, then the total number of unique managers, then a summary of managers by location. The next set of tasks were to display the mean and standard deviation of years of employment by performance group, and finally to display each managers location classification of their office, ID number, and test score in alphabetical order by location type and descending order of test score. Functions used to accomplish these tasks included SELECT, COUNT, AS, FROM, WHERE, GROUP BY, and ORDER BY inside of dbGetQuery to extract the results tables. 
 dbGetQuery(conn, "
   SELECT COUNT(*) AS total_managers 
   FROM week13_tbl 
